40881abd17f528059877aa350cec5b7480d2ebbf
Nabok Serhii <s.nabock@gmail.com>
2021-05-25 21:13:06 +0300

    change html

---
+++ js/modul7.js
@@ -0,0 +1,10 @@
+const feList = document.querySelector('.features-list');
+// console.log('feList', feList);
+
+
+const feLinksList = document.querySelectorAll('.features-list');
+// console.log('feLinksList', feLinksList);
+
+
+const heroTitleEl = document.querySelector('.features-title');
+heroTitleEl.textContent = 'Пиписька';
